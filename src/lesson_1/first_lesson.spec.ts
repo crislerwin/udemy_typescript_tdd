5bf291018e83609cc383520393ce2643cc16a3a7
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-02-22 21:57:38 -0300

    spec: ensure that the created users will be an object with name and id

--- src/lesson_1/first_lesson.spec.ts
+++ src/lesson_1/first_lesson.spec.ts
@@ -3,16 +3,27 @@ import { expect, test, describe } from "vitest";
 import { beforeEach } from "node:test";
 
 describe("User", () => {
+  const mockUsers = [
+    {
+      name: "John",
+      id: 1,
+    },
+    {
+      name: "Jane",
+      id: 2,
+    },
+  ];
+
   test("greet", () => {
     const sut = new User();
-    expect(sut.greet("John")).toBe("Hello, John!");
+    expect(sut.greet(mockUsers[0].name)).toBe(`Hello, ${mockUsers[0].name}!`);
   });
   test("Create and find users", () => {
     const sut = new User();
     expect(sut.listUsers()).toEqual([]);
-    sut.addUser("John");
-    expect(sut.listUsers()).toEqual(["John"]);
-    sut.addUser("Jane");
-    expect(sut.listUsers()).toEqual(["John", "Jane"]);
+    sut.addUser(mockUsers[0].name);
+    expect(sut.listUsers()).toEqual([mockUsers[0]]);
+    sut.addUser(mockUsers[1].name);
+    expect(sut.listUsers()).toEqual(mockUsers);
   });
 });
