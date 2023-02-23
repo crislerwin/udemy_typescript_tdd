99e6f8175e0ffbc4170cb059a5c0c8f4db31cdab
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-02-22 21:41:51 -0300

    chore: create and get users list

--- src/lesson_1/first_lesson.ts
+++ src/lesson_1/first_lesson.ts
@@ -1,5 +1,13 @@
 export class User {
+  private users: string[] = [];
   greet(name: string) {
     return `Hello, ${name}!`;
+  }
+  listUsers() {
+    return this.users;
+  }
+
+  addUser(name: string) {
+    this.users.push(name);
   }
 }
