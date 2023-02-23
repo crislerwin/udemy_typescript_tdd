b827a15f5e65a0af29915b0da6db4677b37b13ef
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-02-22 21:43:03 -0300

    spec: ensure that it will create and fetch created users

--- src/lesson_1/first_lesson.spec.ts
+++ src/lesson_1/first_lesson.spec.ts
@@ -1,9 +1,18 @@
 import { User } from "./first_lesson";
 import { expect, test, describe } from "vitest";
+import { beforeEach } from "node:test";
 
 describe("User", () => {
   test("greet", () => {
     const sut = new User();
     expect(sut.greet("John")).toBe("Hello, John!");
+  });
+  test("Create and find users", () => {
+    const sut = new User();
+    expect(sut.listUsers()).toEqual([]);
+    sut.addUser("John");
+    expect(sut.listUsers()).toEqual(["John"]);
+    sut.addUser("Jane");
+    expect(sut.listUsers()).toEqual(["John", "Jane"]);
   });
 });
